3dfe13de4afb3e9246a77efd54386de84312bb97
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-01-31 09:39:43 +0100

    add html style

---
+++ src/js/settings.js
@@ -0,0 +1,12 @@
+export const select = {};
+export const classNames = {};
+export const settings = {
+  db: {
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    products: 'products',
+  },
+};
+export const templates = {};
